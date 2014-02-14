43111a85dd21cff334cc54bbeeb60ceaf08f6724
Justin Eldridge <eldridgejm@gmail.com>
2014-02-14 08:51:47 -0500

    Initial commit.

---
+++ doc/modules.h
@@ -0,0 +1,43 @@
+
+
+/// \defgroup graph_implementations Graph Implementations
+/// \brief implementations
+
+    /// \defgroup graph_implementations_structures Structures
+    /// \ingroup graph_implementations
+    
+    /// \defgroup graph_implementations_mixins Mixins
+    /// \ingroup graph_implementations
+
+    /// \defgroup graph_implementations_iterators Iterators
+    /// \ingroup graph_implementations
+
+    /// \defgroup graph_implementations_maps Maps
+    /// \ingroup graph_implementations
+
+
+/// \defgroup contour_tree Contour Tree
+
+
+/// \defgroup concepts Concepts
+
+    /// \defgroup concepts_graph Graph Concepts
+    /// \ingroup concepts
+
+
+        /// \defgroup concepts_graph_attributes Attributes
+        /// \ingroup concepts_graph
+        //
+        /// \defgroup concepts_graph_objects Objects
+        /// \ingroup concepts_graph
+
+        /// \defgroup concepts_graph_iterators  Iterators
+        /// \ingroup concepts_graph
+
+        /// \defgroup concepts_graph_maps Maps
+        /// \ingroup concepts_graph
+
+    /// \brief Contour tree concepts.
+    /// \defgroup concepts_contour_tree Contour Tree Concepts
+    /// \ingroup concepts
+
