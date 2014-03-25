fec9d8ca6a1843946c6b1dc2090786b0ca540142
Justin Eldridge <eldridgejm@gmail.com>
2014-03-24 22:02:56 -0400

    Added write colors function.

--- python/denali/io.py
+++ python/denali/io.py
@@ -2,8 +2,7 @@ import numpy as _numpy
 import itertools as _itertools
 from StringIO import StringIO as _StringIO
 
-
-def read_selection(fileob):
+def read_selection(fileobj):
     """Read the selection information from a file-like object."""
 
     def _process_filename(data):
@@ -21,7 +20,7 @@ def read_selection(fileob):
             }
 
     # group the lines of the selection file, breaking on #
-    grouping = _itertools.groupby(fileob, lambda x: x.startswith('#'))
+    grouping = _itertools.groupby(fileobj, lambda x: x.startswith('#'))
 
     # zip together the keys with the data groups
     flags = (list(g)[0].lstrip('# ').rstrip('\n') for k,g in grouping if k)
@@ -36,16 +35,25 @@ def read_selection(fileob):
     return selection_information
 
 
-def write_tree(filename, tree):
+def write_tree(fileobj, tree):
     """Writes a tree in denali format."""
-    with open(filename, "w") as f:
-        # write the number of vertices
-        f.write("{}\n".format(len(tree)))
-        
-        # now write each node to the file, along with its value
-        for node in tree:
-            f.write("{}\t{}\n".format(node, tree.node[node]['value']))
-            
-        # and write each edge
-        for u,v in tree.edges_iter():
-            f.write("{}\t{}\n".format(u,v))
+    # write the number of vertices
+    f.write("{}\n".format(len(tree)))
+
+    # now write each node to the file, along with its value
+    for node in tree:
+        f.write("{}\t{}\n".format(node, tree.node[node]['value']))
+
+    # and write each edge
+    for u,v in tree.edges_iter():
+        f.write("{}\t{}\n".format(u,v))
+
+
+def write_colors_from_arrays(fileobj, ids, values):
+    """Writes a color map in denali format. 
+    
+    Input is specified as two arrays: the first being the list of IDs, the
+    second being the scalar values corresponding to the IDs in the first
+    array."""
+    data = _numpy.column_stack((ids, values))
+    _numpy.savetxt(fileobj, data, fmt="%d\t%f")
