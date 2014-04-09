944447d88d9eac31633b3616802f3193b6eec0b3
Justin Eldridge <eldridgejm@gmail.com>
2014-04-09 14:49:02 -0400

    Moved to using git to upload.

--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SERVER="eldridge@stdlinux.cse.ohio-state.edu"
-DIRECTORY="WWW/denali"
-
-rsync -a index.html ${SERVER}:${DIRECTORY}
-rsync -a resources ${SERVER}:${DIRECTORY}
-
-ssh $SERVER "cd $DIRECTORY && chmod -R 755 *"
