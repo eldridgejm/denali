74f9e2603ea2ed3061c77e6f7438bce3aa528a2c
Justin Eldridge <eldridgejm@gmail.com>
2014-05-12 16:59:01 -0400

    Added comment example.

---
+++ examples/comments/callback.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+
+import denali
+import sys
+import pickle
+import os
+
+# get an absolute path to the data, assuming its in the same 
+# directory as this script
+comments_path = os.path.join(os.path.dirname(__file__), "comments.pickle")
+comments = pickle.load(open(comments_path))
+
+# load the selection file
+selection = denali.io.read_selection(open(sys.argv[1]))
+
+comment_id = selection['component'][1][0]
+
+comment = comments.node[comment_id]
+
+print "<b>Upvotes:</b> {}<br><b>Downvotes:</b> {}<br><b>Body:</b>{}".format(
+            comment['upvotes'],
+            comment['downvotes'],
+            comment['body'])
