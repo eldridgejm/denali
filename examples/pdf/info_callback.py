3a15ca496437772220ffacf0a2f4d0748a5a5a43
Justin Eldridge <eldridgejm@gmail.com>
2014-05-16 11:41:50 -0400

    Moved to read_selection_file.

--- examples/pdf/info_callback.py
+++ examples/pdf/info_callback.py
@@ -8,7 +8,7 @@ import numpy as np
 import denali
 
 # read the selection information
-selection = denali.io.read_selection(open(sys.argv[1]))
+selection = denali.io.read_selection_file(sys.argv[1])
 
 # extract the component
 u,v = (x[0] for x in selection['component'])
